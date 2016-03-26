76547d0405a504a68dbbb9db1af5e85bb9db969e
YuTanaka <YRK00337@nifty.com>
2016-03-26 23:29:48 +0900

    DBUnitテスト動作

--- server/test/db/DbTest.php
+++ server/test/db/DbTest.php
@@ -23,39 +23,58 @@ class ErrorTable extends Eloquent {
 
 class DbTest extends \PHPUnit_Extensions_Database_TestCase
 {
-    public $pdo_conn = null;
+    public static $pdo = null;
+    var $pdo_conn = null;
+    var $capsule = null;
+    var $settings;
 
     /**
      * TestCaseからデータベースへの接続
      */
     public function getConnection() {
         if ($this->pdo_conn == null) {
-            $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', TEST_DB_USER, TEST_DB_PASS);
-            $this->pdo_conn = $this->createDefaultDBConnection($pdo);
+            if (self::$pdo == null) {
+                self::$pdo = new PDO('sqlite::memory:');
+                //self::$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', TEST_DB_USER, TEST_DB_PASS);
+            }
+
+            //$this->pdo_conn = $this->createDefaultDBConnection(self::$pdo);
+            $this->pdo_conn = $this->createDefaultDBConnection(self::$pdo, ':memory:');
         }
         return $this->pdo_conn;
     }
 
     /**
-     * テスト用のデータセットを作成する
+     * テスト用のデータセットを作成する。最初の１回だけ実行
      */
     public function getDataSet() {
+        // 初期化
+        $this->settings = require __DIR__ . '/../../src/settings-debug.php';
+
+        $this->capsule = new Capsule;
+
+        //$this->capsule->addConnection($this->settings['settings']['db']['config']);
+        $this->capsule->addConnection(
+        [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ]);
+        $this->capsule->setAsGlobal();
+        $this->capsule->bootEloquent();
+
+        // データセットを返す
+        //return $this->createFlatXMLDataSet(__DIR__.'/init-error-data.xml');
         return new PHPUnit_Extensions_Database_DataSet_YamlDataSet(
             __DIR__ . '/init-error-data.yml'
         );
     }
 
-    /**
+    /**相棒
      * DBに接続
      */
-    public function testConnection() {
-        $settings = require __DIR__ . '/../../src/settings-debug.php';
-
-        $capsule = new Capsule;
-
-        $capsule->addConnection($settings['settings']['db']['config']);
-        $capsule->setAsGlobal();
-
+    public function _testConnection() {
         $results = Capsule::table(TABLE_ERROR)->get();
 
         $this->assertNotNull($results);
@@ -64,27 +83,32 @@ class DbTest extends \PHPUnit_Extensions_Database_TestCase
     /**
      * データの登録テスト
      */
-    public function testInsert() {
-        $settings = require __DIR__ . '/../../src/settings-debug.php';
-
-        $capsule = new Capsule;
-
-        $capsule->addConnection($settings['settings']['db']['config']);
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
-
+    public function _testInsert() {
         // 追加
         $err = new ErrorTable;
-        $err->keycode = Am1Util::makeRandWords($settings['settings']['app']['KEYCODE_LENGTH']);
+        $err->keycode = Am1Util::makeRandWords($this->settings['settings']['app']['KEYCODE_LENGTH']);
         $err->description = "YuTanaka";
         $err->save();
+
+        // 予想
+        $expected = new PHPUnit_Extensions_Database_DataSet_YamlDataSet(
+            __DIR__ . '/init-error-data.yml'
+        );
+
+        // チェック
+        $this->assertDataSetsEqual($expected, $dataSet);
     }
 
     /**
      * データの確認テスト
      */
     public function testListAll() {
+        $errs = ErrorTable::all();
+        foreach($errs as $k => $v) {
+            echo "[$k]=$v\n";
+        }
 
+        $this->assertEquals(1, $this->getConnection()->getRowCount("error_data"), "entry data test");
     }
 }
  ?>
