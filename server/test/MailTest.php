113b80375854c11e5a9b1b6995de5125471b4c9a
YuTanaka <YRK00337@nifty.com>
2016-04-02 13:49:19 +0900

    mailtestの整形2

--- server/test/MailTest.php
+++ server/test/MailTest.php
@@ -8,14 +8,16 @@ class MailTest extends \PHPUnit_Framework_TestCase
     {
     }
 
-    public function xtestSendMail() {
+    public function xtestSendMail()
+    {
         mb_language("Japanese");
         mb_internal_encoding("UTF-8");
         if (mb_send_mail(
-            TEST_TO_ADDR,
-            "件名",
-            "送信テスト",
-            "From :".mb_encode_mimeheader("テスト送信元")."<".TEST_FROM_ADDR.">"))
+        TEST_TO_ADDR,
+        "件名",
+        "送信テスト",
+        "From :".mb_encode_mimeheader("テスト送信元")."<".TEST_FROM_ADDR.">"
+        ))
         {
             $this->assertEquals(0, 0);
         } else {
